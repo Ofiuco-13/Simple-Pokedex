9a9e1fa20b2800e37b0f1cfa9cdfd9eb28253a64
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-03-10 17:00:15 -0300

    Changes prettier tab width

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    content: ["./index.html", "./src/index.js"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: ["./index.html", "./src/index.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
