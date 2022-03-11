2d6d285d03f372d0d3573a101331cae663e59baf
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-03-11 19:48:59 -0300

    Adds functions to show pokemon from the api

--- src/index.js
+++ src/index.js
@@ -26,10 +26,34 @@ const showPokemonList = (results) => {
     cursor-pointer
     `;
     $pokemon.addEventListener("click", () => {
-      console.log(pokemon.url);
+      loadPokemon(pokemon.name);
     });
     $listContainer.appendChild($pokemon);
   });
+};
+
+const loadPokemon = (pokeName) => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+    .then((res) => res.json())
+    .then((res) => {
+      showPokemon(res);
+    });
+};
+
+const showPokemon = (pokemon) => {
+  const {
+    id,
+    sprites: { front_default },
+  } = pokemon;
+
+  const $pokeDetails = document.querySelector("#pokemon");
+  const $pokeId = document.createElement("span");
+  $pokeId.innerHTML = id;
+
+  const $pokeImg = document.createElement("img");
+  $pokeImg.setAttribute("src", `${front_default}`);
+
+  $pokeDetails.append($pokeId, $pokeImg);
 };
 
 start();
