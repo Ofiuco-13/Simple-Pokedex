fe5b1540c2ba7405303d3fa8f7c0cd6f5e8bc52f
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-03-14 19:11:00 -0300

    Adds http-server url in the if condition

--- src/interface/buttons.js
+++ src/interface/buttons.js
@@ -8,7 +8,10 @@ export const showButtons = (res) => {
   $prevButton.href = res.previous;
   $nextButton.href = res.next;
 
-  if ($prevButton.href === "http://127.0.0.1:5500/null") {
+  if (
+    $prevButton.href === "http://127.0.0.1:5500/null" ||
+    $prevButton.href == "http://127.0.0.1:8080/null"
+  ) {
     $prevButton.classList = "hidden";
   } else {
     $prevButton.classList.remove("hidden");
