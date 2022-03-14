e079cd2dc685ed168b9d85e99fdf96f093602a96
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-03-14 19:10:20 -0300

    Adds tests

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -24,11 +24,44 @@ describe("Pokedex", () => {
     });
   });
 
+  it("verifies if the prev button is hidden when in first page", () => {
+    cy.get("#prev-button").should("have.class", "hidden");
+  });
+
+  it("show total pokemon in header", () => {
+    cy.get("#total-pokemon").should("have.text", "1126");
+  });
+
+  it("loads pokemon list", () => {
+    const pokemonsPerPage = 20;
+    cy.get(".pokemon").should("have.length", pokemonsPerPage);
+  });
+
   it("loads a pokemon when selected from the list", () => {
-    const typesQuantity = 2;
-    cy.intercept(
-      "https://pokeapi.co/api/v2/pokemon/bulbasaur",
-      "fixture:bulbasaur"
-    ).as("obtainBulbasaur");
+    cy.intercept("https://pokeapi.co/api/v2/pokemon/bulbasaur").as(
+      "obtainBulbasaur"
+    );
+    cy.fixture("bulbasaur");
+    cy.get(".pokemon").eq(0).click();
+    cy.get(".pokemon").eq(1).click();
+  });
+
+  it("changes to the next page", () => {
+    cy.get("#next-button").click();
+  });
+
+  it("shows the prev button", () => {
+    cy.get("#prev-button").should("be.visible");
+  });
+
+  it("loads selected pokemons", () => {
+    cy.get(".pokemon").eq(19).click();
+    cy.get(".pokemon").eq(9).click();
+    cy.get(".pokemon").eq(17).click();
+  });
+
+  it("goes back to the first page", () => {
+    cy.get("#prev-button").click();
+    cy.get(".pokemon").eq(5).click();
   });
 });
