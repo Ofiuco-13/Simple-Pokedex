6f29dc7a656f77c5b81ee56e8a590c1e399c32b8
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-03-14 15:41:44 -0300

    Adds mockups and fixture

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -1,8 +1,34 @@
 /// <reference types="Cypress" />
 
 describe("Pokedex", () => {
+  let fetchPolyfill;
+
   before(() => {
-    cy.visit("http://127.0.0.1:8080");
+    const polifillUrl = "https://unpkg.com/unfetch@4.2.0/dist/unfetch.umd.js";
+
+    cy.request(polifillUrl).then((response) => {
+      fetchPolyfill = response.body;
+    });
+
+    cy.intercept(
+      "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20",
+      "fixture:listing-page-1"
+    ).as("obtainFirstPage");
+
+    cy.visit("http://127.0.0.1:8080", {
+      onBeforeLoad(contentWindow) {
+        delete contentWindow.fetch;
+        contentWindow.eval(fetchPolyfill);
+        contentWindow.fetch = contentWindow.unfetch;
+      },
+    });
   });
-  it("Does something", () => {});
+
+  it("loads a pokemon when selected from the list", () => {
+    const typesQuantity = 2;
+    cy.intercept(
+      "https://pokeapi.co/api/v2/pokemon/bulbasaur",
+      "fixture:bulbasaur"
+    ).as("obtainBulbasaur");
+  });
 });
