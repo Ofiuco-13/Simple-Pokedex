63b551efe3e7da629307144d7556d627913eaab4
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-03-17 15:12:12 -0300

    Adds Jest configuration.

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  verbose: true,
+  rootDir: "src",
+  coverageDirectory: "../coverage",
+  testPathIngorePatterns: ["/node_modules/", ".*fixture.js"],
+  coveragePathIgnorePatterns: ["/node_modules/", ".*fixture.js"],
+};
