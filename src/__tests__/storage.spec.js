06c538f2b609ffcc1c31db20cb61876087c82772
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-03-19 15:55:27 -0300

    Mocks localStorage.

---
+++ src/__tests__/storage.spec.js
@@ -0,0 +1,56 @@
+import { loadPokemon } from "../storage.js";
+
+beforeEach(() => {
+  global.fetch = jest.fn();
+});
+
+const localStorageMock = (() => {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key];
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+  };
+})();
+
+describe("tests loadPokemon function", () => {
+  test("throws error if pokemon is undefined", async () => {
+    expect.assertions(2);
+    try {
+      return await loadPokemon(undefined);
+    } catch (error) {
+      expect(error.message).toBe("A name is needed to load a pokemon");
+      expect(global.fetch).toHaveBeenCalledTimes(0);
+    }
+  });
+  test("if pokemon is null, fetch the original api", async () => {
+    try {
+      return await localStorageMock.getItem(null);
+    } catch (error) {
+      global.fetch.mockImplementationOnce(() => {
+        new Promise((resolve) => {
+          const jsonPromise = new Promise((r) => {
+            r({});
+          });
+          resolve({ json: () => jsonPromise });
+        });
+      });
+      loadPokemon("bulbasur");
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(global.fetch).toHaveBeenCalledWith(
+        "https://pokeapi.co/api/v2/pokemon/bulbasaur"
+      );
+
+      localStorageMock.setItem("bulbasaur", "bulbasaur");
+    }
+  });
+});
