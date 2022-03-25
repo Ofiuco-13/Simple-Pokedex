03188335a8c640223320d0851bff6a3d9bdcec7e
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-03-25 02:02:16 -0300

    Adds %+90 code coverage.

--- src/interface/__tests__/list.spec.js
+++ src/interface/__tests__/list.spec.js
@@ -2,20 +2,29 @@
  * @jest-environment jsdom
  */
 
-import { showPokemonList } from "../list.js";
-import { loadPokemon } from "../../storage.js";
+import { showPokemonList, showTotalPokemon, showPokemon } from "../list.js";
 import page1 from "../../../cypress/fixtures/page1.json";
+import bulbasaur from "../../../cypress/fixtures/bulbasaur.json";
 import fixture from "../../__tests__/pokedex.fixture.js";
 
-test("foo", () => {
+document.body.innerHTML = fixture;
+
+test("tests showPokemonList function", () => {
   showPokemonList(page1.results);
-  document.body.innerHTML = fixture;
-  expect(document.querySelector("#list-container").children.length).toBe(20);
-  expect(document.querySelector("#list-container").children[0].innerHTML).toBe(
-    "bulbasaur"
-  );
-  expect(document.querySelector("#list-container").children[3].innerHTML).toBe(
-    "charmander"
-  );
-  loadPokemon(page1.results.name);
+  expect(document.querySelectorAll("#list-container .pokemon").length).toBe(40);
+  expect(
+    document.querySelectorAll("#list-container .pokemon")[0].innerHTML
+  ).toBe("bulbasaur");
+  expect(
+    document.querySelectorAll("#list-container .pokemon")[3].innerHTML
+  ).toBe("charmander");
+});
+
+test("tests showTotalPokemon function", () => {
+  showTotalPokemon(page1.count);
+  expect(document.querySelector("#total-pokemon").innerHTML).toBe("1126");
+});
+
+test("tests showPokemon function", () => {
+  showPokemon(bulbasaur);
 });
